9f62c3be32efee94a17a27bcd753d836c3111ed7
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-01-31 18:46:14 +0700

    uploaded code

---
+++ src/routes/backpack/permissionmanager.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::group([
+    'namespace'  => 'Backpack\PermissionManager\app\Http\Controllers',
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => ['web', backpack_middleware()],
+], function () {
+    //
+});
