3b4a1956d9c0d004c63d7287f48a05926b27c5b6
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-01-31 15:21:26 +0700

    uploaded code

--- src/database/seeders/packageSeeder.php
+++ src/database/seeders/packageSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Permission;
-use App\Models\Role;
+use Backpack\NewsCRUD\app\Models\Permission;
+use Backpack\NewsCRUD\app\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
