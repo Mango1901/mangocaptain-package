89ac38dfe47c1a147fd785b6b5803251d1e913d9
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-01-31 15:49:48 +0700

    uploaded code

--- src/app/Http/Controllers/Admin/Operations/ImpersonateOperation.php
+++ src/app/Http/Controllers/Admin/Operations/ImpersonateOperation.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Operations;
+namespace Backpack\NewsCRUD\app\Http\Controllers\Admin\Operations;
 
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
