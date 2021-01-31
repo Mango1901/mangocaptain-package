e1f942d22758ccd64f04998506cc0dc66e1853ff
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-01-31 16:05:34 +0700

    uploaded code

--- src/app/Http/Controllers/Admin/Charts/WeeklyUsersChartController.php
+++ src/app/Http/Controllers/Admin/Charts/WeeklyUsersChartController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Charts;
+namespace Backpack\NewsCRUD\app\Http\Controllers\Admin\Charts;
 
 use App\Models\User;
 use Backpack\CRUD\app\Http\Controllers\ChartController;
