8edddf940fbb3bfc6e7cd3fcc1071d9d5080da07
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-02-01 16:21:26 +0700

    fixed Seeder

--- src/database/seeders/packageSeeder.php
+++ src/database/seeders/packageSeeder.php
@@ -1,9 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
-use Backpack\NewsCRUD\app\Models\Permission;
-use Backpack\NewsCRUD\app\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -17,64 +14,8 @@ class packageSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Role::insert([
-           [
-           "name"=>"Admin",
-            "guard_name"=>"web",
-            "created_at"=>date("Y-m-d H:i:s"),
-            "updated_at"=>date("Y-m-d H:i:s")
-           ],
-            [
-                "name"=>"User",
-                "guard_name"=>"web",
-                "created_at"=>date("Y-m-d H:i:s"),
-                "updated_at"=>date("Y-m-d H:i:s")
-            ],
-            [
-                "name"=>"Author",
-                "guard_name"=>"web",
-                "created_at"=>date("Y-m-d H:i:s"),
-                "updated_at"=>date("Y-m-d H:i:s")
-            ]
-        ]);
-        Permission::insert([
-           [
-               "name"=>"All Privileges",
-               "guard_name"=>"web",
-               "created_at"=>date("Y-m-d H:i:s"),
-               "updated_at"=>date("Y-m-d H:i:s")
-           ],
-            [
-                "name"=>"show",
-                "guard_name"=>"web",
-                "created_at"=>date("Y-m-d H:i:s"),
-                "updated_at"=>date("Y-m-d H:i:s")
-            ],
-            [
-                "name"=>"edit",
-                "guard_name"=>"web",
-                "created_at"=>date("Y-m-d H:i:s"),
-                "updated_at"=>date("Y-m-d H:i:s")
-            ]
-
-        ]);
-        \DB::table("model_has_roles")->insert([
-           [
-               "role_id"=>"1",
-               "model_type"=>"App\Models\User",
-               "model_id"=>"1"
-           ],
-            [
-                "role_id"=>"2",
-                "model_type"=>"App\Models\User",
-                "model_id"=>"2"
-            ],
-            [
-                "role_id"=>"3",
-                "model_type"=>"App\Models\User",
-                "model_id"=>"3"
-            ],
-        ]);
+      
+     
         User::insert([
            [
              "name"=>"Nguyễn Thị Huyền",
@@ -98,19 +39,5 @@ class packageSeeder extends Seeder
                 "updated_at"=>date("Y-m-d H:i:s")
             ]
         ]);
-        \DB::table("role_has_permissions")->insert([
-            ["permission_id"=>1,
-            "role_id"=>1
-            ],
-            ["permission_id"=>2,
-                "role_id"=>2
-            ],
-            ["permission_id"=>2,
-                "role_id"=>3
-            ],
-            ["permission_id"=>3,
-                "role_id"=>3
-            ],
-            ]);
     }
 }
