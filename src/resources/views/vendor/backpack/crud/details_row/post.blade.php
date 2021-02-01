54287b3c476039fcc8908d1399614bde70c142aa
Mango1901 <hieu.tuhai2001@gmailc.om>
2021-02-01 10:09:43 +0700

    fixed config

--- src/resources/views/vendor/backpack/crud/details_row/post.blade.php
+++ src/resources/views/vendor/backpack/crud/details_row/post.blade.php
@@ -4,7 +4,6 @@
             <small>Use the <span class="label label-default">details_row</span> functionality to show more information about the entry, when that information does not fit inside the table column.</small><br><br>
             <strong>Title:</strong> {{ $entry->title }} <br>
             <strong>Description:</strong> {!! $entry->description !!} <br>
-            <strong>Format:</strong> {{ $entry->format }} <br>
             <strong>created_at:</strong> {{ $entry->created_at }} <br>
             <strong>Excerpt:</strong> {{ $entry->excerpt }} <br>
             <strong>Url:</strong> {{ $entry->url }} <br>
